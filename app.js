02525ae8b4e50fb4e88c20f2f7309fcad4fcd37a
Kishor Rathva <kishor.rathva@rapidops.com>
2019-02-20 19:02:18 +0530

    customerAPP

---
+++ app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+
+var app = express();
+
+// var logger = (req,res,next) => {
+//     console.log('Logging ... ');
+//     next();
+// }
+
+// app.use(logger);
+
+//View Engine 
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+//Body Parser Middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false }));
+
+//Set static Path 
+app.use(express.static(path.join(__dirname,'public')));
+app.use(expressValidator());
+
+
+//Global vars
+
+app.use((req,res,next)=>{
+    res.locals.errors = null ;
+    next();
+});
+var users = [
+    {   
+        id : 1 ,
+        first_name : 'john',
+        last_name:'Doe',
+        email:'johndoe@gmail.com'
+    },
+    {   
+        id : 2 ,
+        first_name : 'kishor',
+        last_name:'Rathva',
+        email:'kishorrathva@gmail.com'
+    },
+    {   
+        id : 3 ,
+        first_name : 'nts',
+        last_name:'eys',
+        email:'ntseys@gmail.com'
+    }
+]
+
+app.get('/',(req,res) => {
+    res.render('index',{
+        title:'Customers',
+        users: users
+    });
+});
+
+app.post('/users/add',(req,res) => {
+
+    req.checkBody('first_name','First Name is Required').notEmpty();
+    req.checkBody('last_name','Last Name is Required').notEmpty();
+    req.checkBody('email','Email is Required').notEmpty();
+    
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('index',{
+            title:'Customers',
+            users: users,
+            errors:errors
+        });
+    }else{
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name:req.body.last_name,
+            email:req.body.email
+        }
+
+        console.log('SUCCESS')
+    }
+    
+});
+app.listen(3000, () => {
+    console.log('Server Started on Port 3000....');
+})
+
