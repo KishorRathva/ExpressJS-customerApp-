6a94e4d9db4c69f3275d1589844018bf07dce452
Kishor Rathva <kishor.rathva@rapidops.com>
2019-02-21 17:46:52 +0530

    Ajax DELETE not found ERROR

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var expressValidator = require('express-validator');
-
+var mongojs = require('mongojs');
+var db = mongojs('customerapp', ['users']);
+var ObjectId =  mongojs.ObjectId ;
 var app = express();
 
 // var logger = (req,res,next) => {
@@ -52,10 +54,15 @@ var users = [
 ]
 
 app.get('/',(req,res) => {
-    res.render('index',{
-        title:'Customers',
-        users: users
-    });
+    // find everything
+    db.users.find(function (err, docs) {
+       
+        res.render('index',{
+            title:'Customers',
+            users: docs
+        });
+    })
+    
 });
 
 app.post('/users/add',(req,res) => {
@@ -79,9 +86,26 @@ app.post('/users/add',(req,res) => {
             email:req.body.email
         }
 
-        console.log('SUCCESS')
+        db.users.insert(newUser,(err,result) => {
+            if(err){
+                console.error(err);
+            }
+            res.redirect('/')
+        });
+
+        // console.log('SUCCESS')
     }
     
+});
+
+app.delete('/user/delete/:id', (req,res) => {
+    console.log(req.params.id);
+    db.users.remove({_id : ObjectId(req.params.id)},(err,result) => {
+        if(err){
+            console.log(err);
+        }
+        res.redirect('/');
+    })
 });
 app.listen(3000, () => {
     console.log('Server Started on Port 3000....');
---
+++ public/Js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+    $('#deleteUser').on('click',deleteUser);
+});
+
+deleteUser = () => {
+    var confirmation = confirm('Are You Sure?');
+
+    if(confirmation){
+        $.ajax({
+            method:"DELETE",
+            url:'/users/delete/'+$("#deleteUser").attr('data-id'),
+            success : function( data) {
+                console.log('success');
+             },
+            error : function() {
+              console.log('error');
+      
+            }
+    
+        }).done(response => {
+            window.location.replace('/');
+        });
+    }else{
+        return false;
+    }
+}
